3f89b34ba4acf04862b19646ea73058c448fcd2b
JoseMMA88 <33572798+JoseMMA88@users.noreply.github.com>
2018-04-25 20:14:07 +0200

    Clase Alien update

--- Alien.h
+++ Alien.h
@@ -15,24 +15,55 @@
 #ifndef ALIEN_H
 #define ALIEN_H
 
+#include "Bala.h"
 #include "Clock.h"
 #include "Sprite.h"
 #include "Vector2f.h"
+#include "Texture.h"
+#include "RenderWindow.h"
+#include "Nave.h"
+#include <math.h>
 
 class Alien {
 public:
-    Alien();
+    Alien(m2D::Texture &textura, int tipo);
     Alien(const Alien& orig);
     virtual ~Alien();
+    
+    int getX();
+    int getY();
+    
+   void setPos(int x, int y);
+    
+    void draw(m2D::RenderWindow &window);
+    
+    void dispara(m2D::Texture &textura);
+    
+    void dibujaBalas();
+    
+    void Update();//EL que llamamos en NIVEL
+    
+    void move(float grados);
+    m2D::Vector2f getPos();
+    
+    m2D::Sprite& getShape();
+    
+    int getTipo();
 private:
     int type;
+    
     m2D::Sprite sprite;
+    m2D::Texture texture;
     m2D::Vector2f position;
+    std::vector<Bala*> balas;//vector balas
+    float grados; //grados de movimiento
     int state;
     int attack;
     int bullet; //tipo de bala
-    m2D::Clock clock;
+    m2D::Clock dis;
 };
+
+
 
 #endif /* ALIEN_H */
 
