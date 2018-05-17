c3d0e783d05acb7b31ea293652bd66a9f68b45cf
Angel Terol <egenad8@gmail.com>
2018-05-17 12:46:11 +0200

    Add files via upload

---
+++ Nivel.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Nivel.cpp
+ * Author: raquel
+ * 
+ * Created on 26 de marzo de 2018, 21:32
+ */
+
+#include "Nivel.h"
+
+Nivel::Nivel() {
+}
+
+Nivel::Nivel(const Nivel& orig) {
+}
+
+Nivel::~Nivel() {
+}
+
---
+++ Tienda.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Tienda.cpp
+ * Author: raquel
+ * 
+ * Created on 26 de marzo de 2018, 21:31
+ */
+
+#include "Tienda.h"
+
+Tienda::Tienda() {
+}
+
+Tienda::Tienda(const Tienda& orig) {
+}
+
+Tienda::~Tienda() {
+}
+
