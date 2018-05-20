480967075d1f8dd6fe8d30d32da60c33ecd52553
JoseMMA88 <33572798+JoseMMA88@users.noreply.github.com>
2018-05-20 19:37:49 +0200

    Add files via upload

--- build/Debug/GNU-Linux/RenderWindow.o.d
+++ build/Debug/GNU-Linux/RenderWindow.o.d
@@ -1,5 +1,6 @@
 build/Debug/GNU-Linux/RenderWindow.o: RenderWindow.cpp RenderWindow.h \
- Motor2D.h Event.h Sprite.h Texture.h View.h Vector2f.h Text.h Font.h
+ Motor2D.h Event.h Sprite.h Texture.h View.h Vector2f.h Text.h Font.h \
+ Base.h Estado.h Clock.h Music.h Menu.h Missions.h
 
 RenderWindow.h:
 
@@ -18,3 +19,15 @@ Vector2f.h:
 Text.h:
 
 Font.h:
+
+Base.h:
+
+Estado.h:
+
+Clock.h:
+
+Music.h:
+
+Menu.h:
+
+Missions.h:
---
+++ build/Debug/GNU-Linux/_ext/b958e922/StrategyB2.o.d
@@ -0,0 +1,61 @@
+build/Debug/GNU-Linux/_ext/b958e922/StrategyB2.o: \
+ ../ApolloX-2.1/StrategyB2.cpp ../ApolloX-2.1/StrategyB2.h \
+ ../ApolloX-2.1/Strategy.h ../ApolloX-2.1/Bala.h ../ApolloX-2.1/Texture.h \
+ ../ApolloX-2.1/Motor2D.h ../ApolloX-2.1/Vector2f.h \
+ ../ApolloX-2.1/RenderWindow.h ../ApolloX-2.1/Event.h \
+ ../ApolloX-2.1/Sprite.h ../ApolloX-2.1/View.h ../ApolloX-2.1/Text.h \
+ ../ApolloX-2.1/Font.h ../ApolloX-2.1/Clock.h ../ApolloX-2.1/Game.h \
+ ../ApolloX-2.1/Estado.h ../ApolloX-2.1/Menu.h ../ApolloX-2.1/Music.h \
+ ../ApolloX-2.1/Nave.h ../ApolloX-2.1/Meteorito.h ../ApolloX-2.1/NPC.h \
+ ../ApolloX-2.1/Alien.h ../ApolloX-2.1/Alien2.h ../ApolloX-2.1/Escudo.h \
+ ../ApolloX-2.1/Municion.h ../ApolloX-2.1/Material.h
+
+../ApolloX-2.1/StrategyB2.h:
+
+../ApolloX-2.1/Strategy.h:
+
+../ApolloX-2.1/Bala.h:
+
+../ApolloX-2.1/Texture.h:
+
+../ApolloX-2.1/Motor2D.h:
+
+../ApolloX-2.1/Vector2f.h:
+
+../ApolloX-2.1/RenderWindow.h:
+
+../ApolloX-2.1/Event.h:
+
+../ApolloX-2.1/Sprite.h:
+
+../ApolloX-2.1/View.h:
+
+../ApolloX-2.1/Text.h:
+
+../ApolloX-2.1/Font.h:
+
+../ApolloX-2.1/Clock.h:
+
+../ApolloX-2.1/Game.h:
+
+../ApolloX-2.1/Estado.h:
+
+../ApolloX-2.1/Menu.h:
+
+../ApolloX-2.1/Music.h:
+
+../ApolloX-2.1/Nave.h:
+
+../ApolloX-2.1/Meteorito.h:
+
+../ApolloX-2.1/NPC.h:
+
+../ApolloX-2.1/Alien.h:
+
+../ApolloX-2.1/Alien2.h:
+
+../ApolloX-2.1/Escudo.h:
+
+../ApolloX-2.1/Municion.h:
+
+../ApolloX-2.1/Material.h:
