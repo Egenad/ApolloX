6da10f3a8ab19e2c37a2ac6bcd2a4698fe754017
shiryuko <carlamd1997@hotmail.com>
2018-05-21 17:42:25 +0200

    a falta del ultimo jefe

---
+++ build/Debug/GNU-Linux/Bala.o.d
@@ -0,0 +1,22 @@
+build/Debug/GNU-Linux/Bala.o: Bala.cpp Bala.h Texture.h Motor2D.h \
+ Vector2f.h RenderWindow.h Event.h Sprite.h View.h Text.h Font.h
+
+Bala.h:
+
+Texture.h:
+
+Motor2D.h:
+
+Vector2f.h:
+
+RenderWindow.h:
+
+Event.h:
+
+Sprite.h:
+
+View.h:
+
+Text.h:
+
+Font.h:
---
+++ build/Debug/GNU-Linux/Resultado.o.d
@@ -0,0 +1,72 @@
+build/Debug/GNU-Linux/Resultado.o: Resultado.cpp Resultado.h Sprite.h \
+ Motor2D.h Texture.h Estado.h Music.h Font.h Text.h Clock.h \
+ RenderWindow.h Event.h View.h Vector2f.h Nave.h Bala.h Meteorito.h NPC.h \
+ Alien.h Alien2.h Escudo.h Municion.h Material.h Game.h Menu.h Partida.h \
+ Jefe.h Strategy.h StrategyA.h StrategyB.h StrategyA2.h StrategyB2.h \
+ StrategyC.h Missions.h
+
+Resultado.h:
+
+Sprite.h:
+
+Motor2D.h:
+
+Texture.h:
+
+Estado.h:
+
+Music.h:
+
+Font.h:
+
+Text.h:
+
+Clock.h:
+
+RenderWindow.h:
+
+Event.h:
+
+View.h:
+
+Vector2f.h:
+
+Nave.h:
+
+Bala.h:
+
+Meteorito.h:
+
+NPC.h:
+
+Alien.h:
+
+Alien2.h:
+
+Escudo.h:
+
+Municion.h:
+
+Material.h:
+
+Game.h:
+
+Menu.h:
+
+Partida.h:
+
+Jefe.h:
+
+Strategy.h:
+
+StrategyA.h:
+
+StrategyB.h:
+
+StrategyA2.h:
+
+StrategyB2.h:
+
+StrategyC.h:
+
+Missions.h:
